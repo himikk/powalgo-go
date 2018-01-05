a13c6aaa5862d6be63fdbfeb2467060a9c366e30
Isaac Cook <isaac@simpload.com>
2018-01-05 17:06:38 -0600

    Allocate structures properly. Add test

--- x17.go
+++ x17.go
@@ -1,13 +1,15 @@
 package powalgo
 
 /*
+#include <stdlib.h>
 #include "x17.h"
 */
 import "C"
 
 func X17hash(input []byte) ([]byte, error) {
-	var output *C.char
+	var output = C.malloc(32)
+	defer C.free(output)
 	in := C.CString(string(input))
-	C.crypto_hash(output, in)
-	return []byte(C.GoString(output)), nil
+	C.crypto_hash(in, (*C.char)(output))
+	return C.GoBytes(output, 32), nil
 }
